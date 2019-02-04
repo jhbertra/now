0e24d861a5fb3c7dd2bfb12327d736c9363a2467
Jamie Bertram <jhbertra@gmail.com>
2019-02-04 17:21:37 -0500

    Add init commands

--- Now/Program.fs
+++ Now/Program.fs
@@ -1,24 +1,44 @@
-﻿open FSharpPlus.Builders
-open Now.Environment
+﻿open FSharpPlus.Operators
+open Now
 
+
+type Command =
+| InitGlobal
+| InitLocal
+
+
+type Error =
+| Environment of Environment.Error
+| CommandLineInvalid
+
+
+let parseCommand = function
+| "init" :: opts when opts |> List.contains "-g" -> Ok InitGlobal
+| "init" :: opts -> Ok InitLocal
+| _ -> Error CommandLineInvalid
+
+
+let runCommand = function
+| InitGlobal -> Environment.initGlobalDir
+| InitLocal -> Environment.initLocalDir
+
+open Environment
 
 let renderEnvironmentError = function
-| IoError exn -> sprintf "IO Error: %s" exn.Message
-| GlobalDirExists -> "The global Now directory already exists"
-| LocalDirExists -> "A local Now directory already exists"
+| CommandLineInvalid -> "Command Line Invalid"
+| Environment (IoError exn) -> sprintf "IO Error: %s" exn.Message
+| Environment (GlobalDirExists) -> "The global Now directory already exists"
+| Environment (LocalDirExists) -> "A local Now directory already exists"
 
 
 [<EntryPoint>]
-let main argv =
-    monad {
-        do! initLocalDir
-        let! environment = readEnvironment
-        printfn "%A" environment
-    }
-    |> interpret
-    |> (function    
-        | Ok () -> 0
-        | Error e ->
-            printfn "%s" (renderEnvironmentError e)
-            -1
-       )
+let main argv =    
+    let result =
+        parseCommand (List.ofArray argv)
+        |> map runCommand
+        >>= (interpret >> Result.mapError Environment)
+    match result with
+    | Ok () -> 0
+    | Error e ->
+        printfn "%s" (renderEnvironmentError e)
+        -1
